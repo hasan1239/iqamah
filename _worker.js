4b134ce1c0d5889d1946c225b9d2d41d4b9802aa
Hasan Tariq <htariq0786@gmail.com>
2026-03-03 00:49:35 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,19 +5,31 @@ export default {
 
     // Try serving static asset first
     const response = await env.ASSETS.fetch(request);
-
-    // If found, return it
     if (response.status !== 404) {
       return response;
     }
 
-    // If 404, try clean URL: /aisha -> serve masjid.html with ?id=aisha
+    // If 404, try clean URL: /aisha -> serve masjid.html content
+    // getMasjidId() in masjid.html reads the slug from the URL path
     const segment = path.replace(/^\//, '').replace(/\/$/, '');
     if (segment && !segment.includes('.') && !segment.includes('/')) {
-      const newUrl = new URL(request.url);
-      newUrl.pathname = '/masjid.html';
-      newUrl.searchParams.set('id', segment);
-      return env.ASSETS.fetch(new Request(newUrl, request));
+      const assetUrl = new URL(request.url);
+      assetUrl.pathname = '/masjid.html';
+      let res = await env.ASSETS.fetch(assetUrl.toString());
+
+      // Follow redirect if Cloudflare strips .html
+      if (res.status >= 300 && res.status < 400) {
+        const loc = res.headers.get('Location');
+        if (loc) {
+          res = await env.ASSETS.fetch(new URL(loc, assetUrl).toString());
+        }
+      }
+
+      // Return content at the clean URL
+      return new Response(res.body, {
+        status: 200,
+        headers: res.headers
+      });
     }
 
     return response;
